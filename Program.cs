c68e2a143a78386c66220e4f42d1916b00d3f390
DougSilvera <shortxjdoug@gmail.com>
2022-03-11 15:53:57 -0500

    exercises

--- Program.cs
+++ Program.cs
@@ -8,13 +8,54 @@ namespace ShootingDice
     {
         static void Main(string[] args)
         {
+            SoreLoserUpperHalfPlayer cheatingButNotSatisfied = new SoreLoserUpperHalfPlayer();
+            cheatingButNotSatisfied.Name = "Nobody's favorite opponent";
+
+            UpperHalfPlayer upperDecker = new UpperHalfPlayer();
+            upperDecker.Name = "always on top";
+
+            SoreLoserPlayer neverGonnaLose = new SoreLoserPlayer();
+            neverGonnaLose.Name="Your little sibling";
+
+           
+
+            CreativeSmackTalkingPlayer IntellectualInsulter = new CreativeSmackTalkingPlayer();
+            IntellectualInsulter.Name = "Buzz Killington";
+            IntellectualInsulter.tauntBank.Add("Away, you starvelling, you elf-skin, you dried neat’s-tongue, bull’s-pizzle, you stock-fish!");
+            IntellectualInsulter.tauntBank.Add("I am sick when I do look on thee");
+            IntellectualInsulter.tauntBank.Add("I’ll beat thee, but I would infect my hands.");
+            IntellectualInsulter.tauntBank.Add("More of your conversation would infect my brain.");
+            HumanPlayer MrRoboto = new HumanPlayer();
+            MrRoboto.Name = "Wall-E";
+
+            OneHigherPlayer cheaterCheaterPumpkinEater = new OneHigherPlayer();
+            cheaterCheaterPumpkinEater.Name= "Sore Winner";
+            try
+            {
+            neverGonnaLose.Play(cheaterCheaterPumpkinEater);
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            SmackTalkingPlayer smackPlayer = new SmackTalkingPlayer();
+            smackPlayer.Name="Jury Byrd";
+            smackPlayer.Taunt="Your mom goes to college!";
+            
+            IntellectualInsulter.Play(smackPlayer);
+            Console.WriteLine("-------------------");
+            cheaterCheaterPumpkinEater.Play(smackPlayer);
+
+            Console.WriteLine("-------------------");
             Player player1 = new Player();
             player1.Name = "Bob";
 
             Player player2 = new Player();
             player2.Name = "Sue";
-
-            player2.Play(player1);
+            smackPlayer.Play(player2);
+            Console.WriteLine("-------------------");
+            player2.Play(smackPlayer);
 
             Console.WriteLine("-------------------");
 
@@ -32,11 +73,23 @@ namespace ShootingDice
 
             Console.WriteLine("-------------------");
 
+            cheaterCheaterPumpkinEater.Play(large);
+
+            Console.WriteLine("-------------------");
+            cheatingButNotSatisfied.Play(player1);
+            Console.WriteLine("-------------------");
+
+            MrRoboto.Play(player1);
+           
+            Console.WriteLine("-------------------");
+
+
             List<Player> players = new List<Player>() {
-                player1, player2, player3, large
+                player1, player2, player3, large, smackPlayer
             };
 
             PlayMany(players);
+            upperDecker.Play(player1);
         }
 
         static void PlayMany(List<Player> players)
